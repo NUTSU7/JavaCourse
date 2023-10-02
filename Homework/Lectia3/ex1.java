de7c10b3db0b1f1c16e378b01fe67d09251e4baa
NUTSU7 <Bivolliviu62@gmail.com>
2023-10-02 18:55:37 +0300

    Homework lesson 3 pt2
    Signed-off-by: NUTSU7 <Bivolliviu62@gmail.com>

---
+++ Homework/Lectia3/ex1.java
@@ -0,0 +1,9 @@
+public class ex1 {
+    public static void main(String[] args) {
+        String months[] = { "Ianuarie", "Februarie", "Martie", "Aprilie", "Mai", "Iunie", "Iulie", "August",
+                "Septembrie", "Octombrie", "Noiembrie", "Decembrie" };
+        for(int i=0; i<12; i++){
+            System.out.println(months[i]);
+        }
+    }
+}
